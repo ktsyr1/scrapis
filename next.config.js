9a7f32ba66fb474058f13d712024bab9ad64a02b
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2023-01-24 06:52:44 +0200

    pwa

---
+++ next.config.js
@@ -0,0 +1,16 @@
+
+const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+});
+module.exports = withPWA({
+    reactStrictMode: true,
+    // i18n: {
+    //     locales: ['en', 'ar'],
+    //     defaultLocale: 'ar'
+    // }
+})
